25eb468a64f490a1aa2c01c1b83c7d8e71951f8a
Stream <1542763342@qq.com>
2025-06-11 08:52:53 +0800

    feat: init

---
+++ build.gradle.kts
@@ -0,0 +1,125 @@
+@file:OptIn(ExperimentalWasmDsl::class, ExperimentalKotlinGradlePluginApi::class)
+
+import com.vanniktech.maven.publish.SonatypeHost
+import org.jetbrains.kotlin.gradle.ExperimentalKotlinGradlePluginApi
+import org.jetbrains.kotlin.gradle.ExperimentalWasmDsl
+import org.jetbrains.kotlin.gradle.dsl.JsModuleKind
+import org.jetbrains.kotlin.gradle.plugin.mpp.KotlinNativeTarget
+import org.jetbrains.kotlin.gradle.plugin.mpp.KotlinNativeTargetWithTests
+import org.jetbrains.kotlin.gradle.plugin.mpp.NativeBuildType
+
+plugins {
+    kotlin("multiplatform") version "2.1.21"
+    id("com.vanniktech.maven.publish") version "0.29.0"
+}
+
+group = "io.github.stream29"
+version = "0.0.1-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+kotlin {
+    explicitApi()
+    jvm()
+    jvmToolchain(8)
+
+    js {
+        nodejs {
+            testTask {
+                useMocha {
+                    timeout = "10s"
+                }
+            }
+        }
+
+        compilerOptions {
+            sourceMap = true
+            moduleKind = JsModuleKind.MODULE_UMD
+        }
+    }
+
+    wasmJs {
+        nodejs()
+    }
+    wasmWasi {
+        nodejs()
+    }
+
+    linuxArm64()
+    macosX64()
+    macosArm64()
+    iosSimulatorArm64()
+    iosX64()
+
+    // Tier 2
+    linuxX64()
+    linuxArm64()
+    watchosSimulatorArm64()
+    watchosX64()
+    watchosArm32()
+    watchosArm64()
+    tvosSimulatorArm64()
+    tvosX64()
+    tvosArm64()
+    iosArm64()
+
+    // Tier 3
+    mingwX64()
+    watchosDeviceArm64()
+    androidNativeArm32()
+    androidNativeArm64()
+    androidNativeX86()
+    androidNativeX64()
+
+    targets.withType<KotlinNativeTarget>().configureEach {
+        binaries.test(listOf(NativeBuildType.RELEASE))
+    }
+    targets.withType<KotlinNativeTargetWithTests<*>>().configureEach {
+        testRuns.create("releaseTest") {
+            setExecutionSourceFrom(binaries.getTest(NativeBuildType.RELEASE))
+        }
+    }
+    applyDefaultHierarchyTemplate {}
+
+    sourceSets {
+        commonMain {}
+        commonTest {
+            dependencies {
+                implementation(kotlin("test"))
+            }
+        }
+    }
+}
+
+mavenPublishing {
+    publishToMavenCentral(SonatypeHost.CENTRAL_PORTAL)
+    signAllPublications()
+    coordinates((group as String), "regexdsl", version.toString())
+    pom {
+        name.set("RegexDsl")
+        description.set("A DSL way to write regex in KMP.")
+        inceptionYear.set("2025")
+        url.set("https://github.com/Stream29/RegexDsl")
+        licenses {
+            license {
+                name.set("Apache License Version 2.0")
+                url.set("https://www.apache.org/licenses/LICENSE-2.0")
+                distribution.set("https://www.apache.org/licenses/LICENSE-2.0")
+            }
+        }
+        developers {
+            developer {
+                id.set("Stream29")
+                name.set("Stream")
+                url.set("https://github.com/Stream29/")
+            }
+        }
+        scm {
+            url.set("https://github.com/Stream29/RegexDsl")
+            connection.set("scm:git:git://github.com/Stream29/RegexDsl.git")
+            developerConnection.set("scm:git:ssh://git@github.com:Stream29/RegexDsl.git")
+        }
+    }
+}
