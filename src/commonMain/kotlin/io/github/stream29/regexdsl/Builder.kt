f6e07bc1eac56a3316628e3f3c1d4b597985fbd5
Stream <1542763342@qq.com>
2025-06-12 15:19:09 +0800

    feat: add `buildRegexString` for constructing regex as string

--- src/commonMain/kotlin/io/github/stream29/regexdsl/Builder.kt
+++ src/commonMain/kotlin/io/github/stream29/regexdsl/Builder.kt
@@ -8,12 +8,17 @@ package io.github.stream29.regexdsl
  * 
  * @param block A lambda with [RegexScope] as the receiver that defines the regular expression.
  * @return A [Regex] object representing the defined pattern.
- * 
- * @sample io.github.stream29.regexdsl.BuilderTest.testBuildSimpleRegex
- * @sample io.github.stream29.regexdsl.BuilderTest.testBuildComplexRegex
  */
-public inline fun buildRegex(block: RegexScope.() -> Unit): Regex {
+public inline fun buildRegex(block: RegexScope.() -> Unit): Regex = Regex(buildRegexString(block))
+
+/**
+ * Builds a string representation of a [Regex] object using the RegexDSL.
+ *
+ * This is a convenience function for building regular expressions without creating a [Regex] object.
+ * It is useful for passing regular expressions to APIs that expect a string representation.
+*/
+public inline fun buildRegexString(block: RegexScope.() -> Unit): String {
     val sb = StringBuilder()
     RegexScope().apply(block).appendTo(sb)
-    return sb.toString().toRegex()
+    return sb.toString()
 }
